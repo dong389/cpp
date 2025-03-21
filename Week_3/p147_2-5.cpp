b84b7cd50d8cbc9253a4579a13e69a1a707676f2
dong389 <ehdrb217@naver.com>
2025-03-21 12:09:06 +0900

    dk

---
+++ Week_3/p147_2-5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "p147_2-5.h"
+
+int reverse(int);
+int main() {
+    int number;
+    std::cout << "정수를 입력하세요: ";
+    std::cin >> number;
+
+    std::cout << "역순 결과: " << reverse(number) << std::endl;
+
+    return 0;
+}
+int reverse(int n) {
+    int reversed = 0;
+    while (n != 0) {
+        int digit = n % 10; // 마지막 자리 숫자 추출
+        reversed = reversed * 10 + digit; // 뒤집은 숫자 구성
+        n /= 10; // 마지막 자리 제거
+    }
+    return reversed;
+}
